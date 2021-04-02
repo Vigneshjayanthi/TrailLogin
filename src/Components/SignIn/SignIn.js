6212db672c4250f321923bf2ee2a1a291d1501d4
Vigneshjayanthi <vikkilebronjames@gmail.com>
2021-04-02 21:01:59 +0530

    signup small change

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -16,8 +16,8 @@ class signIn extends Component {
      
     submitHandler = (e) => {
         e.preventDefault()
-        console.log(e.target.elements.username.value," ",e.target.elements.password.value)
-        if(e.target.elements.username.value==="vignesh" && e.target.elements.password.value==="Vikki@6498"){
+        console.log(this.state.Username," ",this.state.password)
+        if(this.state.Username ==="vignesh" && this.state.password ==="Vikki@6498"){
                 this.props.history.push('/');
         }
         else{
@@ -36,11 +36,12 @@ class signIn extends Component {
                 <form onSubmit={(event) => { this.submitHandler(event) }}>
                     <div className="form-group">
                         <b> <label htmlFor="name">Username:</label></b>
-                        <input type="text" name="username" id="name" className="form-control" onChange={(event)=>{this.setState({Username:{...event.target.value}})}}></input>
+                        <input type="text" name="username" id="name" className="form-control" 
+                        onChange={(event)=>{this.setState({Username:event.target.value})}}></input>
                     </div>
                     <div className="form-group">
                         <b> <label htmlFor="password">Password:</label></b>
-                        <input type="password" name="password" id="password" className="form-control" onChange={(event)=>{this.setState({password:{...event.target.value}})}}></input>
+                        <input type="password" name="password" id="password" className="form-control" onChange={(event)=>{this.setState({password:event.target.value})}}></input>
                     </div>
                     <div className="form-group">
                         <button type="submit">Submit</button>
